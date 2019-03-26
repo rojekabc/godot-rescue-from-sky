7caca162756d8d8fe0b2c7d1672545f1b688444f
Piotr Rojewski <rojek_abc@o2.pl>
2019-03-26 14:40:12 +0100

    Issue #29. Fix border power compare

--- Objects/Tool.gd
+++ Objects/Tool.gd
@@ -18,6 +18,38 @@ func array_min(array : PoolRealArray) -> float:
 	for value in array:
 		result = min(value, result)
 	return result
+
+func array_max_idx(array : PoolRealArray) -> int:
+	var result : int = 0
+	var found = array[result]
+	for idx in range(0, array.size()):
+		if array[idx] > found:
+			result = idx
+			found = array[idx]
+	return result
+	
+func array_min_idx(array : PoolRealArray) -> int:
+	var result : int = 0
+	var found = array[result]
+	for idx in range(0, array.size()):
+		if array[idx] < found:
+			result = idx
+			found = array[idx]
+	return result
+
+func array_min_max_idx(array : PoolRealArray) -> PoolIntArray:
+	var idxmin : int = 0
+	var idxmax : int = 0
+	var foundmin = array[idxmin]
+	var foundmax = array[idxmax]
+	for idx in range(0, array.size()):
+		if array[idx] > foundmax:
+			idxmax = idx
+			foundmax = array[idx]
+		if array[idx] < foundmin:
+			idxmin = idx
+			foundmin = array[idx]
+	return PoolIntArray([idxmin, idxmax])
 	
 func _ready():
 	pass
